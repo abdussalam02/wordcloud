d552e7849be27547fca11233f0c5aa0b411119cc
abdussalam02 <samaniabdussalam02@gmail.com>
2021-12-23 23:56:15 +0530

    frequencies

--- wordcloud_demo.py
+++ wordcloud_demo.py
@@ -10,6 +10,7 @@ import io
 # creating upload widget
 def _upload():
     _upload_widget = fileupload.FileUploadWidget()
+
     def _cb(change):
         global file_contents
         decoded = io.StringIO(change['owner'].data.decode('utf-8'))
@@ -21,3 +22,27 @@ def _upload():
     display(_upload_widget)
     
 _upload()
+
+# calculating frequencies
+def calculate_frequencies():
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+    "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being", \
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+
+    frequencies = {}
+    file_contents = file_contents.split()
+    str1 = ""
+    for word in file_contents:
+        str1 = "".join(ch for ch in word if ch.isalnum())
+        if str1.lower() not in uninteresting_words:
+            if str1.lower() not in frequencies:
+                frequencies[str1.lower()] = 1
+            else:
+                frequencies[str1.lower()] += 1
+
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(frequencies)
+    return cloud.to_array()
